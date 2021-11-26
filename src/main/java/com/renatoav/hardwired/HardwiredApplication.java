370cdd57950adea427b726c98e4b7e253e860d35
renato <renatoavila10@hotmail.com>
2021-11-26 19:26:41 -0300

    initial commit

---
+++ src/main/java/com/renatoav/hardwired/HardwiredApplication.java
@@ -0,0 +1,13 @@
+package com.renatoav.hardwired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwiredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renatoav/hardwired/HardwiredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatoav.hardwired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwiredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
